d7adc1beafa88186e556187e2018c508f358f47a
nastya-koptilova <koptilova.nastya@gmail.com>
2022-12-22 22:56:30 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+const onBodyChangeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+};
+
+changeColorBtn.addEventListener("click", onBodyChangeColor);
