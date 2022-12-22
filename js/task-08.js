d734479fee66938d68ccf67a4ea3a04ce9f7aaf2
nastya-koptilova <koptilova.nastya@gmail.com>
2022-12-22 22:45:40 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginFormEl = document.querySelector(".login-form");
+const mailFormEl = loginFormEl.elements.email;
+const passwordFormEl = loginFormEl.elements.password;
 
+const submitLoginForm = (event) => {
+  if (mailFormEl.value === "" || passwordFormEl.value === "") {
+    alert("Please fill out all filds!");
+  } else {
+    const userData = {
+      email: mailFormEl.value,
+      password: passwordFormEl.value,
+    };
+    console.log(userData);
+    loginFormEl.reset();
+  }
+  event.preventDefault();
+};
+
+loginFormEl.addEventListener("submit", submitLoginForm);
