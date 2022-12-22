0b11eff2fcf6e05daedccb28443db4b851f343be
nastya-koptilova <koptilova.nastya@gmail.com>
2022-12-22 02:32:38 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const makeIngredientsList = function () {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.classList.add("item");
+  return ingredientsItem;
+};
+
+const ingredientsAddEl = ingredients.map(() => makeIngredientsList());
+
+const ingredientsAddText = function () {
+  for (let i = 0; i < ingredients.length; i += 1) {
+    ingredientsAddEl[i].textContent = ingredients[i];
+  }
+  return ingredientsAddEl;
+};
+
+ingredientsAddText();
+
+ingredientsEl.append(...ingredientsAddEl);
