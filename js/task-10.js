6f9069c638c458018395a672c1ea0323263bf243
nastya-koptilova <koptilova.nastya@gmail.com>
2022-12-24 17:54:43 +0200

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -8,26 +8,21 @@ const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxesEl = document.querySelector("#boxes");
 
-const createAmount = () => Number(inputEl.value);
-
 const addBoxes = (width, height) => `<div style="width: ${width}px; height: ${height}px; background-color: ${getRandomHexColor()}"></div>`;
 
-const createBoxes = (amount) => {
+const createBoxes = () => {
   const arr = [];
   let width = 20;
   let height = 20;
   for (let i = 0; i < Number(inputEl.value); i += 1) {
-    arr.push(addBoxes());
     width += 10;
     height += 10;
-    boxesEl.insertAdjacentHTML("beforeend", addBoxes(width, height));
+    arr.push(addBoxes(width, height));
   }
+  boxesEl.insertAdjacentHTML("beforeend", arr.join(""));
 };
 
 const destroyBoxes = () => (boxesEl.innerHTML = "");
 
-createBoxes(Number(inputEl.value));
-
 createBtn.addEventListener("click", createBoxes);
-inputEl.addEventListener("input", createAmount);
 destroyBtn.addEventListener("click", destroyBoxes);
